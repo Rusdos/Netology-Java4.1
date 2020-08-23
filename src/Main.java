c4c513d79fbd7a74b7eb29b68ca272cfb33df2ca
Rusdos <mr.Nurgaleev@gmail.com>
2020-08-24 00:40:04 +0500

    Update console output

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 8000; /*стоимость билета 8000 руб. */
+        double cost = 8000.50; /*стоимость билета */
         int promotion = 20; /*сумма, которую нужно потратить для начисления 1 мили*/
-        int miles = cost/promotion; /* количество бонусных миль*/
-        System.out.println("Вам начислено: " + miles + " бонусов");
+        int miles;
+        if (cost >= 0) {
+            miles = (int) cost / promotion; /* количество бонусных миль*/
+            System.out.println("Бонусов начислено: " + miles);
+        } else {
+            System.out.println("Стоимость билета не может быть отрицательной. Пожалуйста, введите положительное число.");
+        }
+
+
     }
 }
